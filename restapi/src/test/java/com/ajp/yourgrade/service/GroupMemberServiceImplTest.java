f654c8dc2c0f4289b1eaac529604714c6213316d
Ronald Elzen <ronald_elzen@hotmail.com>
2018-01-21 19:59:13 +0100

    Tests for UserService

--- restapi/src/test/java/com/ajp/yourgrade/service/GroupMemberServiceImplTest.java
+++ restapi/src/test/java/com/ajp/yourgrade/service/GroupMemberServiceImplTest.java
@@ -1,42 +1,71 @@
 package com.ajp.yourgrade.service;
 
+import com.ajp.yourgrade.model.Group;
+import com.ajp.yourgrade.model.GroupMember;
+import com.ajp.yourgrade.model.Template;
+import com.ajp.yourgrade.model.User;
 import com.ajp.yourgrade.persistence.GroupMemberRepository;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito.*;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
-class GroupMemberServiceImplTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+public class GroupMemberServiceImplTest {
+
     @Mock
-    GroupMemberRepository groupMemberRepository;
+    private GroupMemberRepository groupMemberRepository;
+
+    @Mock
+     Group group;
+
+    @Mock
+    private Template template;
+    @Mock
+    private User user;
 
     @InjectMocks
     private GroupMemberServiceImpl groupMemberServiceImpl;
 
-    @Test
-    void createMember() {
+    @Before
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+        group = Mockito.mock(Group.class);
     }
 
     @Test
-    void deleteMember() {
+    public void createMember() {
     }
 
     @Test
-    void testGetMembersByGroup() {
-
-
+    public void deleteMember() {
     }
 
     @Test
-    void getMemberById() {
+    public void testGetMembersByGroup() {
+//        List<GroupMember> userList = new ArrayList<>();
+//        GroupMember groupMember = new GroupMember("John","johndoe@gmail.com","adf",true,group);
+//        GroupMember groupMember2 = new GroupMember("John","johndoe@hotmail.com","adf",true,group);
+//        userList.add(groupMember);
+//        userList.add(groupMember2);
+//        when(groupMemberRepository.findByGroup(group)).thenReturn(userList);
     }
 
     @Test
-    void getMemberByToken() {
+    public void getMemberById()  throws Exception{
+
+        GroupMember groupMember = new GroupMember("John","johndoe@gmail.com","adf",true,group);
+        when(groupMemberRepository.findById(0)).thenReturn(new GroupMember("John","johndoe@gmail.com","adf",true,group));
+    }
+
+    @Test
+    public void getMemberByToken() {
     }
 }
--- restapi/src/test/java/com/ajp/yourgrade/service/UserServiceImplTest.java
+++ restapi/src/test/java/com/ajp/yourgrade/service/UserServiceImplTest.java
@@ -2,20 +2,6 @@ package com.ajp.yourgrade.service;
 
 import com.ajp.yourgrade.model.User;
 import com.ajp.yourgrade.persistence.UserRepository;
-import com.ajp.yourgrade.persistence.UserTokenRepository;
-import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,14 +10,14 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 public class UserServiceImplTest {
 
     @Mock
     private UserRepository userRepository;
-
-    @Mock
-    private UserTokenRepository userTokenRepository;
 
     @InjectMocks
     private UserServiceImpl userService;
@@ -44,14 +30,11 @@ public class UserServiceImplTest {
 
     @Test
     public void addUser() {
-        userService.addUser("adf", "adf.nl", true, "adf", "nl");
-        User result = userService.getUserById(0);
-        assertEquals(0,result.getId());
+
     }
 
     @Test
     public void deleteUser() {
-        when(userRepository.findById(0)).thenReturn(new User("af", "adf.nl", true, "adf", "nl"));
 
     }
 
