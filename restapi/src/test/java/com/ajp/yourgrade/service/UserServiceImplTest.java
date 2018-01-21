e7edd260beaf0ca21d8d07bd952ed63ec5a91180
joshmar <joshmarsteff@gmail.com>
2018-01-21 20:06:40 +0100

    Delete en adduser afgerond

--- restapi/src/test/java/com/ajp/yourgrade/service/UserServiceImplTest.java
+++ restapi/src/test/java/com/ajp/yourgrade/service/UserServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.ajp.yourgrade.service;
 
 import com.ajp.yourgrade.model.User;
+<<<<<<< HEAD
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -11,8 +12,15 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.internal.verification.VerificationModeFactory.only;
 
+=======
+import com.ajp.yourgrade.persistence.UserRepository;
+import org.junit.Before;
+>>>>>>> c8c896a41c71bc19c209ee006481dc1e85451991
 import org.junit.Test;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class UserServiceImplTest {
@@ -20,7 +28,14 @@ public class UserServiceImplTest {
     private User u = new User("af", "adf.nl", true, "adf", "nl");
 
     @Mock
+<<<<<<< HEAD
     private UserServiceImpl userServiceImpl;
+=======
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+>>>>>>> c8c896a41c71bc19c209ee006481dc1e85451991
 
     @Before
     public void setup() {
@@ -36,14 +51,26 @@ public class UserServiceImplTest {
 
     @Test
     public void deleteUser() {
+<<<<<<< HEAD
         userServiceImpl.deleteUser(0);
         verify(userServiceImpl, only()).deleteUser(0);
+=======
+
+>>>>>>> c8c896a41c71bc19c209ee006481dc1e85451991
     }
 
     @Test
     public void getUserById() {
+<<<<<<< HEAD
         when(userServiceImpl.getUserById(0)).thenReturn(u);
         User result = userServiceImpl.getUserById(0);
+=======
+        //Give value that will be returned when a user is searched in the repo (id)
+        when(userRepository.findById(0)).thenReturn(new User("af", "adf.nl", true, "adf", "nl"));
+        //Get a user from the service
+        User result = userService.getUserById(0);
+        //Check if the values are correct.
+>>>>>>> c8c896a41c71bc19c209ee006481dc1e85451991
         assertEquals(0,result.getId());
         assertEquals("af", result.getName());
         assertEquals("adf.nl", result.getEmail());
