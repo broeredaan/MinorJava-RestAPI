6007d78302abda2ee2830ca53b63084a25e844da
joshmar <joshmarsteff@gmail.com>
2018-01-23 13:29:00 +0100

    GroupService Testclasse finished

--- restapi/src/test/java/com/ajp/yourgrade/service/GroupServiceImplTest.java
+++ restapi/src/test/java/com/ajp/yourgrade/service/GroupServiceImplTest.java
@@ -11,15 +11,16 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.internal.verification.VerificationModeFactory.only;
 
 import org.junit.Test;
+import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -31,38 +32,51 @@ import java.util.List;
     @InjectMocks
     private GroupServiceImpl groupServiceImpl;
 
+    @Mock
+    private GroupServiceImpl groupServiceImplMock;
+
+    @Mock
+    private Template templateMock;
+
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
     }
 
+    private Date creationDate = new java.util.Date(2018, 01, 02, 12, 00, 00 );
+    private Date deadline = new java.util.Date(2018, 01, 30, 12, 00, 00);
+
     @Test
     public void addGroup() {
+        groupServiceImplMock.addGroup("group", creationDate, deadline, 8.0, templateMock, true);
+        verify(groupServiceImplMock, only()).addGroup("group", creationDate, deadline, 8.0, templateMock, true);
     }
 
     @Test
     public void deleteGroup() {
+        groupServiceImplMock.deleteGroup( 0);
+        verify(groupServiceImplMock, only()).deleteGroup( 0);
     }
 
     @Test
     public void getById() {
         Template templateMock = Mockito.mock(Template.class);
         //Give value that will be returned when a user is searched in the repo (id)
-        when(groupRepository.findById(0)).thenReturn(new Group("Test",new java.util.Date(),new java.util.Date(),8,templateMock,true));
+        when(groupRepository.findById(0)).thenReturn(new Group("Test",creationDate,deadline,8,templateMock,true));
         //Get a user from the service
         Group result = groupServiceImpl.getById(0);
         //Check if the values are correct.
         assertEquals(0,result.getId());
         assertEquals("Test", result.getName());
-        assertEquals(new java.util.Date(), result.getCreationDate());
-        assertEquals(new java.util.Date(), result.getDeadline());
+        assertEquals(creationDate, result.getCreationDate());
+        assertEquals(deadline, result.getDeadline());
         assertEquals(8, result.getGroupGrade());
     }
 
     @Test
     public void getByTemplate() {
         Template templateMock = Mockito.mock(Template.class);
-        Group group = new Group("Test",new java.util.Date(),new java.util.Date(),8,templateMock,true);
+        Group group = new Group("Test",creationDate,deadline,8,templateMock,true);
         List<Group> groupList = new ArrayList<>();
         groupList.add(group);
         when(groupRepository.findByTemplate(templateMock)).thenReturn(groupList);
@@ -70,17 +84,21 @@ import java.util.List;
         for (Group resultGroup : result) {
             assertEquals(0,resultGroup.getId());
             assertEquals("Test", resultGroup.getName());
-            assertEquals(new java.util.Date(), resultGroup.getCreationDate());
-            assertEquals(new java.util.Date(), resultGroup.getDeadline());
+            assertEquals(creationDate, resultGroup.getCreationDate());
+            assertEquals(deadline, resultGroup.getDeadline());
             assertEquals(8, resultGroup.getGroupGrade());
         }
     }
 
     @Test
     public void setApproved() {
+        groupServiceImplMock.setApproved(true, 0);
+        verify(groupServiceImplMock, only()).setApproved(true, 0);
     }
 
     @Test
     public void setSend() {
+        groupServiceImplMock.setSend(true, 0);
+        verify(groupServiceImplMock, only()).setSend(true, 0);
     }
 }
---
+++ restapi/src/test/java/com/ajp/yourgrade/service/PdfServiceImplTest.java
@@ -0,0 +1,10 @@
+package com.ajp.yourgrade.service;
+
+import org.junit.Test;
+
+public class PdfServiceImplTest {
+
+    @Test
+    public void print() {
+    }
+}
